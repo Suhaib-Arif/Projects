786110687cc80df9b334271f53d5f0693db38eef
Suhaib-Arif <115211151+Suhaib-Arif@users.noreply.github.com>
2022-10-16 18:38:12 +0530

    auction.py

---
+++ auction/auction.py
@@ -0,0 +1,23 @@
+from replit import clear
+bid_cont=True
+print(logo)
+print("Welcome to the secreat auction program")
+
+bidshanary ={}
+
+def bid_values(self,bid_key,bid_value):
+
+    self[bid_key]=bid_value
+while bid_cont:    
+    name=input("What is your Name :")
+    bid=int(input("please enter your bid :$"))  
+    
+    bid_values(bidshanary,name,bid)
+    YN=input("Is there another bidder Y/N ").upper()
+    if YN =='N':
+        bid_cont=False
+    clear()
+# print("The highest bid is:")
+key_max=max(zip(bidshanary.values(),bidshanary.keys()))[1]
+# print(bidshanary)
+print(f"The highest bid is {bidshanary[key_max]} from {key_max}")
